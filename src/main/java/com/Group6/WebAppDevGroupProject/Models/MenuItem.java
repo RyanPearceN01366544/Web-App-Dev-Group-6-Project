0ca7b8888144d60c31a6a28851d282d90de19d0f
RyanPearceN01366544 <n01366544@humber.ca>
2025-08-06 19:36:32 -0400

    Added variables and functions into Model files.

--- src/main/java/com/Group6/WebAppDevGroupProject/Models/MenuItem.java
+++ src/main/java/com/Group6/WebAppDevGroupProject/Models/MenuItem.java
@@ -1,4 +1,31 @@
 package com.Group6.WebAppDevGroupProject.Models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class MenuItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int item_id;
+    private String name;
+    private String description;
+    private float price;
+    private int stock;
+
+    // -- Basic Getters --
+    public int getItem_id() { return item_id; }
+    public String getName() { return name; }
+    public String getDescription() { return description; }
+    public float getPrice() { return price; }
+    public int getStock() { return stock; }
+
+    // -- Basic Setters --
+    public void setItem_id(int item_id) { this.item_id = item_id; }
+    public void setName(String name) { this.name = name; }
+    public void setDescription(String description) { this.description = description; }
+    public void setPrice(float price) { this.price = price; }
+    public void setStock(int stock) { this.stock = stock; }
 }
--- src/main/java/com/Group6/WebAppDevGroupProject/Models/Order.java
+++ src/main/java/com/Group6/WebAppDevGroupProject/Models/Order.java
@@ -1,4 +1,34 @@
 package com.Group6.WebAppDevGroupProject.Models;
 
+import jakarta.persistence.*;
+import java.util.Date;
+
+@Entity
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int order_id;
+    private int user_id;
+    private String order_items;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date order_time;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date order_requested;
+    private String order_status;
+
+    // -- Basic Getters --
+    public int getOrder_id() { return order_id; }
+    public int getUser_id() { return user_id; }
+    public String getOrder_items() { return order_items; }
+    public Date getOrder_time() { return order_time; }
+    public Date getOrder_requested() { return order_requested; }
+    public String getOrder_status() { return order_status; }
+
+    // -- Basic Setters --
+    public void setOrder_id(int order_id) { this.order_id = order_id; }
+    public void setUser_id(int user_id) { this.user_id = user_id; }
+    public void setOrder_items(String order_items) { this.order_items = order_items; }
+    public void setOrder_time(Date order_time) { this.order_time = order_time; }
+    public void setOrder_requested(Date order_requested) { this.order_requested = order_requested; }
+    public void setOrder_status(String order_status) { this.order_status = order_status; }
 }
