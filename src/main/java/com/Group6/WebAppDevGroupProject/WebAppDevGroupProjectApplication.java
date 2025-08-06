bef84accbd6a27ba4af6ee1f0e881a5b59a5add9
RyanPearceN01366544 <n01366544@humber.ca>
2025-08-06 17:52:10 -0400

    Init

---
+++ src/main/java/com/Group6/WebAppDevGroupProject/WebAppDevGroupProjectApplication.java
@@ -0,0 +1,13 @@
+package com.Group6.WebAppDevGroupProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppDevGroupProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppDevGroupProjectApplication.class, args);
+	}
+
+}
