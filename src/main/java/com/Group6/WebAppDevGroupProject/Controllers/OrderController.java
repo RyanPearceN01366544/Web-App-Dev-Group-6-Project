95b376f1809fcb4e382123e88823a679826b96da
LuckyNinja1 <156844535+RyanPearceN01366544@users.noreply.github.com>
2025-08-09 14:47:14 -0400

    Added a bunch of functionality, redoing OrderController soon and create OrderAdminController for handling REST specifically. Pushing early for transferring to desktop.

--- src/main/java/com/Group6/WebAppDevGroupProject/Controllers/OrderController.java
+++ src/main/java/com/Group6/WebAppDevGroupProject/Controllers/OrderController.java
@@ -2,17 +2,13 @@ package com.Group6.WebAppDevGroupProject.Controllers;
 
 import com.Group6.WebAppDevGroupProject.Models.MenuItem;
 import com.Group6.WebAppDevGroupProject.Models.Order;
-import com.Group6.WebAppDevGroupProject.Models.User;
 import com.Group6.WebAppDevGroupProject.Service.OrderService;
 import com.Group6.WebAppDevGroupProject.Service.MenuService;
-import com.Group6.WebAppDevGroupProject.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
-import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
@@ -21,65 +17,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @Controller
 @RequestMapping("/orders")
 public class OrderController {
+
     @Autowired
     private OrderService orderService;
+
     @Autowired
     private MenuService menuService;
-    @Autowired
-    private UserService userService;
 
-    @GetMapping("/")
-    public String list(Model model_) {
-        //model_.addAttribute("orders", orderService.getAllOrdersFromUser(user.getUser_id()));
-        return "orders-list";
-    }
-    @GetMapping("/active")
-    public String listActiveOrders(Model model_) {
-        //model_.addAttribute("orders", orderService.getAllActiveOrdersFromUser(user.getUser_id()));
-        return "orders-list";
-    }
-    @GetMapping("/{id}")
-    public String getOrder(Model model_, @PathVariable("id") long id) {
-        Order ord_ = orderService.getOrderById(id);
-        /*
-        if (ord_.getUser_id() == user.getUser_id() ||
-            user.getRole().equals("staff") ||
-            user.getRole().equals("admin")) {
-                model_.addAttribute("order", orderService.getOrderById(id));
-                model_.addAttribute("menuItems", orderService.getMenuItemsFromOrder(id));
-        }
-        */
-        return "orders-details";
-    }
-    @GetMapping("/new")
-    public String create(Model model_) {
-        model_.addAttribute("order", new Order());
-//        model_.addAttribute("menuItems", menuService) // -> Solve Later
-        return "orders-create";
-    }
-    @PostMapping("/save")
-    public String editForm(Model model_, @ModelAttribute Order ord_) {
-        orderService.saveOrder(ord_);
-        return "orders-thankyou";
-    }
-    @GetMapping("/edit/{id}")
-    public String editView(Model model_, @PathVariable("id") long id) {
-        /*
-        Order ord_ = orderService.getOrderById(user.getUser_id());
-        if (ord_.getUser_id() == user.getUser_id() || user.getRole().equals("staff") || user.getRole().equals("admin")) {
-            model_.addAttribute("order", orderService.getOrderById(user.getUser_id()));
-        }
-        */
-        return "order-edit";
-    }
-    @PostMapping("/edit")
-    public String edit(@ModelAttribute Order ord_) {
-        orderService.updateOrder(ord_.getOrder_id(), ord_);
-        return "redirect:/orders/";
-    }
-    @GetMapping("/delete/{id}")
-    public String delete(@PathVariable("id") long id) {
-        orderService.deleteOrder(id);
-        return "redirect:/orders/";
-    }
+
 }
